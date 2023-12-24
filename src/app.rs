27a041ba8ace2b21e2cf99a00209ee6e70458fb1
MeiWagatsuma <wagamei256@gmail.com>
2023-12-24 13:12:59 +0900

    some

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,9 @@
-use leptos::*;
-use leptos_meta::*;
-use leptos_router::*;
+use leptos::{
+    component, create_resource, create_signal, expect_context, server, spawn_local, view, IntoView,
+    ServerFnError, SignalGet, SignalUpdate,
+};
+use leptos_meta::{provide_meta_context, Stylesheet, Title};
+use leptos_router::{Route, Router, Routes};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -30,6 +33,10 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
+    // a signal that controls how many cat pics we want
+    let (how_many_cats, set_how_many_cats) = create_signal(1);
+
+    // create a resource that will refetch whenever `how_many_cats` changes
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
     let on_click = move |_| set_count.update(|count| *count += 1);
@@ -37,6 +44,11 @@ fn HomePage() -> impl IntoView {
     view! {
         <h1>"Welcome to Leptos!"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
+        <button on:click=move|_| set_how_many_cats.update(|count| *count +=1 )>"More Cats"</button>
+        <p>"cats: " {how_many_cats}</p>
+        <button on:click=move |_| {  spawn_local(async {
+            say_hello_in_server().await;
+        }) }>"hello"</button>
     }
 }
 
@@ -49,15 +61,21 @@ fn NotFound() -> impl IntoView {
     // if you navigate to the 404 page subsequently, the status
     // code will not be set because there is not a new HTTP request
     // to the server
-    #[cfg(feature = "ssr")]
-    {
-        // this can be done inline because it's synchronous
-        // if it were async, we'd use a server function
-        let resp = expect_context::<leptos_actix::ResponseOptions>();
-        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
-    }
+    // {
+    //     // this can be done inline because it's synchronous
+    //     // if it were async, we'd use a server function
+    //     let resp = expect_context::<leptos_actix::ResponseOptions>();
+    //     resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+    // }
 
     view! {
         <h1>"Not Found"</h1>
     }
+}
+
+#[server]
+pub async fn say_hello_in_server() -> Result<(), ServerFnError> {
+    println!("hello");
+    Err(ServerFnError::Request("hello".to_owned()))
+    // Ok(())
 }
